6e3de80acf47c32a3c68f1f043c0e93a490fa935
Jagan3954 <jagan3954@gmail.com>
2026-05-22 11:56:17 +0530

    new bit

--- rtl/img_pro_top_loop_back_test.v
+++ rtl/img_pro_top_loop_back_test.v
@@ -23,11 +23,11 @@ module img_pro_top_loop_back_test(
     input  wire        s_axi_rready,
 
     // AXI-Stream (Data)
-    input  wire [23:0] s_axis_tdata,
+    input  wire [31:0] s_axis_tdata,
     input  wire        s_axis_tvalid,
     input  wire        s_axis_tlast,   // Added
     output wire        s_axis_tready,
-    output wire [23:0] m_axis_tdata,
+    output wire [31:0] m_axis_tdata,
     output wire        m_axis_tvalid,
     output wire        m_axis_tlast,   // Added
     input  wire        m_axis_tready
